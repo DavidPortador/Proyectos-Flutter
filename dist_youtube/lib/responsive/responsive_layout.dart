4fd700c18ca6cae11cfa25cdf4c3713b0347a0f9
David Portador <62720642+DavidPortador@users.noreply.github.com>
2023-02-27 22:55:26 -0600

    Proyectos a implementar

---
+++ dist_youtube/lib/responsive/responsive_layout.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final mobileWidth = 600;
+  final Widget mobileBody;
+  final Widget desktopBody;
+
+  ResponsiveLayout({required this.mobileBody, required this.desktopBody});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < mobileWidth) {
+          return mobileBody;
+        } else {
+          return desktopBody;
+        }
+      },
+    );
+  }
+}
