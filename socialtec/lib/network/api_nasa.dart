994e0019dfeae477ce70ab1dbc0a30a2a699ad11
David Portador <62720642+DavidPortador@users.noreply.github.com>
2023-05-22 08:22:58 -0600

    fix api nasa bug

--- socialtec/lib/network/api_nasa.dart
+++ socialtec/lib/network/api_nasa.dart
@@ -5,14 +5,13 @@ import 'package:socialtec/models/nasa_model.dart';
 class ApiNasa {
   Future<List<NasaModel>?> getAll(String tipo) async {
     try {
-      print('=> https://api.nasa.gov/EPIC/api/$tipo?api_key=ut9N5GCDqEZI6KTzNkCdFIOtD1hev0cCN1xp2AYs');
+      print(
+          '=> https://api.nasa.gov/EPIC/api/$tipo?api_key=ut9N5GCDqEZI6KTzNkCdFIOtD1hev0cCN1xp2AYs');
       var result = await http.get(Uri.parse(
-        '''https://api.nasa.gov/EPIC/api/$tipo?
-        api_key=ut9N5GCDqEZI6KTzNkCdFIOtD1hev0cCN1xp2AYs'''
-      ));
+          '''https://api.nasa.gov/EPIC/api/$tipo?api_key=ut9N5GCDqEZI6KTzNkCdFIOtD1hev0cCN1xp2AYs'''));
       var listJSON = jsonDecode(result.body) as List;
       if (result.statusCode == 200) {
-        print(listJSON);
+        print('LIST -> $listJSON');
         return listJSON.map((natural) => NasaModel.fromMap(natural)).toList();
       }
       return null;
--- socialtec/lib/network/api_popular.dart
+++ socialtec/lib/network/api_popular.dart
@@ -4,12 +4,10 @@ import 'package:http/http.dart' as http;
 import 'package:socialtec/models/popular_model.dart';
 
 class ApiPopular {
-  
-  Uri link = Uri.parse(
-      'https://api.themoviedb.org/3/movie/popular?api_key=5019e68de7bc112f4e4337a500b96c56&language=es-MX');
 
   Future<List<PopularModel>?> getAllPopular() async {
-    var result = await http.get(link);
+    var result = await http.get(Uri.parse(
+      'https://api.themoviedb.org/3/movie/popular?api_key=5019e68de7bc112f4e4337a500b96c56&language=es-MX'));
     var listJSON = jsonDecode(result.body)['results'] as List;
     if (result.statusCode == 200) {
       return listJSON.map((popular) => PopularModel.fromMap(popular)).toList();
